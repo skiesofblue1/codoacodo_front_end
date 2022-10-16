482b9be5fc8269f4d76c65bc409c5a76ceb7adc1
Alejandra Celeste Ramallal <ac.ramallal@gmail.com>
2022-10-16 20:44:43 -0300

    Agregado archivo Javascript

---
+++ js/compra-tickets.js
@@ -0,0 +1,13 @@
+const estudiante = document.getElementById("estudiante");
+const trainee = document.getElementById("trainee");
+const junior = document.getElementById("junior");
+const nombre = document.getElementById("inputNombre");
+const apellido = document.getElementById("inputApellido");
+const correo = document.getElementById("inputCorreo");
+const cantidad = document.getElementById("inputCantidad");
+const PRECIO_TICKET = 200;
+const PRECIO_ESTUDIANTE = 0.2;
+const PRECIO_TRAINEE = 0.5;
+const PRECIO_JUNIOR = 0.85;
+const precioActual = document.getElementById("precioActual");
+
