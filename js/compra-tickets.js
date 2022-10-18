cc740977e933cbfe7f3f1d63456e9a9b391719c8
Alejandra Celeste Ramallal <ac.ramallal@gmail.com>
2022-10-18 19:15:18 -0300

    Sección Tickets con JS

--- js/compra-tickets.js
+++ js/compra-tickets.js
@@ -1,13 +1,35 @@
-const estudiante = document.getElementById("estudiante");
-const trainee = document.getElementById("trainee");
-const junior = document.getElementById("junior");
-const nombre = document.getElementById("inputNombre");
-const apellido = document.getElementById("inputApellido");
-const correo = document.getElementById("inputCorreo");
-const cantidad = document.getElementById("inputCantidad");
-const PRECIO_TICKET = 200;
+/*
+const inputNombre = document.getElementById("inputNombre");
+const inputApellido = document.getElementById("inputApellido");
+const inputCcorreo = document.getElementById("inputCorreo");
+*/
+const tipoUsuario = document.getElementById("tipoUsuario");
+const inputCantidad = document.getElementById("inputCantidad");
+const resumen = document.getElementById("resumen");
+const reset = document.getElementById("reset");
+let precioTicket = 200;
 const PRECIO_ESTUDIANTE = 0.2;
 const PRECIO_TRAINEE = 0.5;
 const PRECIO_JUNIOR = 0.85;
-const precioActual = document.getElementById("precioActual");
+const precioNeto = document.getElementById("precioNeto");
+
+function precioTotal (e) {
+    e.preventDefault();
+    if (tipoUsuario.value == `1`) {
+        precioTicket = precioTicket * PRECIO_ESTUDIANTE;
+    } else if (tipoUsuario.value == `2`) {
+        precioTicket = precioTicket * PRECIO_TRAINEE;
+    } else {
+        precioTicket = precioTicket * PRECIO_JUNIOR;
+    }
+    precioNeto.innerHTML = `Total a Pagar: $ ${precioTicket * inputCantidad.value}`;
+    precioTicket = 200
+}
+
+function borrarP () {
+    precioNeto.innerHTML = "Total a Pagar: $"
+}
+
+resumen.onclick = precioTotal;
+reset.onclick = borrarP;
 
